acff761cdf3b6b138544cec1c66e6e1113a3b449
Mark McDowall <markus.mcd5@gmail.com>
2015-03-04 07:20:16 -0800

    Use command to import (for progress messaging)

---
+++ src/NzbDrone.Core/MediaFiles/EpisodeImport/Manual/ManualImportCommand.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using NzbDrone.Core.Messaging.Commands;
+
+namespace NzbDrone.Core.MediaFiles.EpisodeImport.Manual
+{
+    public class ManualImportCommand : Command
+    {
+        public string Path { get; set; }
+        public int SeriesId { get; set; }
+        public List<int> EpisodeIds { get; set; }
+        public int QualityId { get; set; }
+    }
+}
---
+++ src/NzbDrone.Core/MediaFiles/EpisodeImport/Manual/ManualImportService.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using NLog;
+using NzbDrone.Common.Extensions;
+using NzbDrone.Core.MediaFiles.MediaInfo;
+using NzbDrone.Core.Messaging.Commands;
+using NzbDrone.Core.Parser.Model;
+using NzbDrone.Core.Qualities;
+using NzbDrone.Core.Tv;
+
+namespace NzbDrone.Core.MediaFiles.EpisodeImport.Manual
+{
+    public class ManualImportService : IExecute<ManualImportCommand>
+    {
+        private readonly ISeriesService _seriesService;
+        private readonly IEpisodeService _episodeService;
+        private readonly IVideoFileInfoReader _videoFileInfoReader;
+        private readonly IImportApprovedEpisodes _importApprovedEpisodes;
+        private readonly Logger _logger;
+
+        public ManualImportService(ISeriesService seriesService,
+                                   IEpisodeService episodeService,
+                                   IVideoFileInfoReader videoFileInfoReader,
+                                   IImportApprovedEpisodes importApprovedEpisodes,
+                                   Logger logger)
+        {
+            _seriesService = seriesService;
+            _episodeService = episodeService;
+            _videoFileInfoReader = videoFileInfoReader;
+            _importApprovedEpisodes = importApprovedEpisodes;
+            _logger = logger;
+        }
+
+        public void Execute(ManualImportCommand message)
+        {
+            var series = _seriesService.GetSeries(message.SeriesId);
+            var episodes = _episodeService.GetEpisodes(message.EpisodeIds);
+            var parsedEpisodeInfo = Parser.Parser.ParsePath(message.Path) ?? new ParsedEpisodeInfo();
+            var quality = new QualityModel(Quality.FindById(message.QualityId));
+            var mediaInfo = _videoFileInfoReader.GetMediaInfo(message.Path);
+            var existingFile = series.Path.IsParentPath(message.Path);
+
+            var localEpisode = new LocalEpisode
+                               {
+                                   ExistingFile = false,
+                                   Episodes = episodes,
+                                   MediaInfo = mediaInfo,
+                                   ParsedEpisodeInfo = parsedEpisodeInfo,
+                                   Path = message.Path,
+                                   Quality = quality,
+                                   Series = series,
+                                   Size = 0
+                               };
+
+            var importDecision = new ImportDecision(localEpisode);
+
+            //TODO: Can we get the downloadClient item? (Lookup by path)?
+            
+            _importApprovedEpisodes.Import(new List<ImportDecision> {importDecision}, !existingFile, null);
+        }
+    }
+}
